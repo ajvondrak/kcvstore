508381b1902d7d414bf54fcd38cd2b1b232acd81
Alex Vondrak <ajvondrak@gmail.com>
2013-11-09 10:42:54 -0800

    kcvstore: first draft implementation based on the bisect module (naive)

--- kcvstore.py
+++ kcvstore.py
@@ -1,3 +1,5 @@
+import bisect
+
 class KeyColumnValueStore(object):
     """A key/column/value store.
 
@@ -16,26 +18,49 @@ class KeyColumnValueStore(object):
     more frequent than writes.
     """
 
+    def __init__(self):
+        self.key_to_cols = {}
+        self.key_to_sorted_cols = {}
+        self.keycol_to_val = {}
+
     def set(self, key, col, val):
         """Sets the value at the given key/column."""
-        pass
+        assert isinstance(key, str)
+        assert isinstance(col, str)
+        assert isinstance(val, str)
+        self.key_to_cols.setdefault(key, set())
+        self.key_to_sorted_cols.setdefault(key, [])
+        if col not in self.key_to_cols[key]:
+            self.key_to_cols[key].add(col)
+            bisect.insort(self.key_to_sorted_cols[key], col)
+        self.keycol_to_val[(key, col)] = val
 
     def get(self, key, col):
         """Return the value at the specified key/column."""
-        pass
+        return self.keycol_to_val.get((key, col))
 
     def get_key(self, key):
         """Returns a sorted list of column/value tuples."""
-        pass
+        sorted_cols = self.key_to_sorted_cols.get(key, [])
+        return [(c, self.get(key, c)) for c in sorted_cols]
 
     def get_keys(self):
         """Returns a set containing all of the keys in the store."""
-        pass
+        return set(self.key_to_cols)
 
     def delete(self, key, col):
         """Removes a column/value from the given key."""
-        pass
+        if (key, col) in self.keycol_to_val:
+            del self.keycol_to_val[(key, col)]
+            self.key_to_cols[key].remove(col)
+            sorted_cols = self.key_to_sorted_cols[key]
+            sorted_cols.pop(bisect.bisect_left(sorted_cols, col))
 
     def delete_key(self, key):
         """Removes all data associated with the given key."""
-        pass
+        if key in self.key_to_cols:
+            cols = self.key_to_cols[key]
+            del self.key_to_cols[key]
+            del self.key_to_sorted_cols[key]
+            for col in cols:
+                del self.keycol_to_val[(key, col)]
